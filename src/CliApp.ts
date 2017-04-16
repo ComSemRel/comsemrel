f828cd5aa44884577478d92fc9b97ed1729f3621
Tomasz Jakut <vepomoc@gmail.com>
2017-04-16 22:47:53 +0200

    refactor(CliApp): implement dummy constructor to satisfy TSLint

--- src/CliApp.ts
+++ src/CliApp.ts
@@ -3,7 +3,9 @@ import { IRenderer } from '@comsemrel/interfaces';
 import ICliApp from './ICliApp';
 
 class CliApp implements ICliApp {
-	constructor( commands: Set<ICommand>, renderer: IRenderer );
+	constructor( commands: Set<ICommand>, renderer: IRenderer ) {
+		// TSLint, Y R U so annoyin' (╯°□°）╯︵ ┻━┻
+	}
 }
 
 export default CliApp;
