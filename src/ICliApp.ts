10e26c3fe66661c46b0cbe2fcb25ddeca60fdb57
Tomasz Jakut <vepomoc@gmail.com>
2017-11-12 17:11:04 +0100

    refactor(TS): get rid of TS

--- src/ICliApp.ts
+++
@@ -1,7 +0,0 @@
-import { ICommand } from '@comsemrel/interfaces';
-import { IRenderer } from '@comsemrel/interfaces';
-
-interface ICliApp {
-}
-
-export default ICliApp;
--- src/Renderer.ts
+++
@@ -1,7 +0,0 @@
-import { IRenderer } from '@comsemrel/interfaces';
-
-class Renderer implements IRenderer {
-
-}
-
-export default Renderer;
